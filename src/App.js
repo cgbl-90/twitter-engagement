10c43bed12cb00432b0d724cc521f855a9605055
carmen <58992828+cgbl-90@users.noreply.github.com>
2022-12-02 15:14:27 +0100

    Summary
    - Created React project
    - Added folders from template
    - Updated readme.md

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import carmen from "./carmen.svg";
+import "./sass/App.css";
+
+// Components inside Main
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+      <header className="flex">
+        <img src={carmen} />
+        <h3>.react | sass.</h3>
+      </header>
+      <main></main>
+      <footer className="flex">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/cgbl-90"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          .github.
         </a>
-      </header>
+        <a href="https://t.me/cg_bl" target="_blank" rel="noopener noreferrer">
+          .let's connect on telegram.
+        </a>
+      </footer>
     </div>
   );
 }
